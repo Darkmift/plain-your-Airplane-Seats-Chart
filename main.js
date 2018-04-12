ee8cee6563dd93ccd680c44e2fe82d9594ba1152
AEK <darkmift@gmail.com>
2018-04-13 01:05:02 +0300

    small fixes removing dead usellee comments

--- main.js
+++ main.js
@@ -1,116 +1,178 @@
+console.log('js online');
+let Column;
+let Row;
+//columns #
+let numColumns;
+//rows #
+let numRows;
+//container of chart
+var container = $('#SeatChartContainer');
+//dimensions for generated chart
+const winVH = $('#SeatChartContainer').height();
+const winVW = $('#SeatChartContainer').width();
+//init seat row
+let SeatRow;
+
 $("form").submit(function(e) {
+    console.log('form clicked');
+    //prevent submit
     e.preventDefault();
+    Column = $('#columns');
+    Row = $('#rows');
+    // bind valus to columns and rows
+    numColumns = Number(Column.val());
+    numRows = Number(Row.val());
 
+    //validate Columns
+    switch (true) {
+        case (numColumns > 10):
+            setAlert(Column, '10 columns is the max allowed.');
+            break;
+        case (numColumns < 4):
+            setAlert(Column, '4 columns is the min allowed.');
+            break;
+        default:
+            break;
 
+    }
+
+    //validate Rows
+    switch (true) {
+        case (numRows < 10):
+            setAlert(Row, '10 rows is the min allowed.');
+            break;
+        case (numRows > 75):
+            setAlert(Row, '75 rows is the max allowed.');
+            break;
+        default:
+            break;
+    }
+
+    //return false if above triggered
+    if (
+        numColumns > 12 ||
+        numColumns < 4 ||
+        numRows > 75 ||
+        numRows < 10
+    ) {
+        return false;
+    }
+
+    //remove previous chart if any
     if ($('.grid-panel')) {
         $('.grid-panel').remove();
     }
 
+    //make panel grid
+    let gridPanel = $('<div>').addClass("grid-panel").css({
+        // "border": "1px solid black",
+        'margin': '3px',
+    });
 
-
-    var numColumns = Number($('.columns').val());
-    var numRows = Number($('.rows').val());
-
-    function num2Array(num) {
-        var n = [];
-        for (var i = 0; i < num; i++) {
-            n.push(i + 1);
-
+    switch (numColumns) {
+        case 4:
+            SeatRow = 'SS_SS';
+            numColumns = numColumns + 1;
+            break;
+        case 5:
+            SeatRow = 'S_SSS_S';
+            numColumns = numColumns + 2;
+            break;
+        case 6:
+            SeatRow = 'SS_SS_SS';
+            numColumns = numColumns + 2;
+            break;
+        case 7:
+            SeatRow = 'SS_SSS_SS';
+            numColumns = numColumns + 2;
+            break;
+        case 8:
+            SeatRow = 'SS_SSSS_SS';
+            numColumns = numColumns + 2;
+            break;
+        case 9:
+            SeatRow = 'SSS_SSS_SSS';
+            numColumns = numColumns + 2;
+            break;
+        case 10:
+            SeatRow = 'SSS_SSSS_SSS';
+            numColumns = numColumns + 2;
+            break;
+    }
+    console.log(numColumns);
+    var seatRowNum = 1;
+    var seatNameNum = 1;
+    for (let i = 0; i < numRows; i++) {
+        for (let index = 0; index < SeatRow.length; index++) {
+            // console.log(SeatRow.charAt(index) + index);
+            switch (SeatRow.charAt(index)) {
+                case 'S':
+                    makeSeat(seatRowNum, seatNameNum, gridPanel, 'Seat');
+                    break;
+                case '_':
+                    makeSeat(seatRowNum, seatNameNum, gridPanel, 'Aisle');
+                    seatNameNum--;
+                    break;
+            }
+            seatNameNum++;
         }
-        return n;
+        seatRowNum++;
+        seatNameNum = 1;
     }
 
-    var numPanels = num2Array(numRows * numColumns);
-
-    var arrColumns = num2Array(numColumns);
-    var arrRows = num2Array(numRows);
-
-    var width = numRows * 80;
-    var height = numColumns * 80;
-
-    var gridPanel = $('<div>').addClass("grid-panel").width(width).height(height);
-    for (var i = 0; i < numPanels.length; i++) {
-        if (i % 3 == 0 && i !== 1) {
-            var panel = $('<div>', {
-                text: 'aisle',
-                class: "panel",
-                click: function(e) {
-                    e.preventDefault();
-                    // var choseChair = $(event.target);
-                    // choseChair.toggleClass('red');
-                },
-            });
-            panel.appendTo(gridPanel);
-        } else {
-            var panel = $('<div>', {
-                text: i + 1,
-                class: "panel",
-                click: function(e) {
-                    e.preventDefault();
-                    var choseChair = $(event.target);
-                    choseChair.toggleClass('red');
-                },
-            });
-            panel.appendTo(gridPanel);
-        }
-        gridPanel.appendTo('.container');
-    }
-
-
-
-    var btns = $('<div>', {
-        class: "input-group-btn",
-        css: {
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "1rem"
-        },
-    });
-
-    var priceBtn = $('<button>', {
-        class: "price btn btn-default",
-        text: "Price",
-        css: { marginRight: "1rem" },
-        click: function(e) {
-
-            if ($('bold')) {
-                $('bold').remove();
-            }
-            var onlyPrice = 100;
-            var multiPrice = 200;
-            var totalPrice = 0;
-
-            var numChairs = Number($('*.red').length);
-            console.log(jQuery.type(numChairs));
-
-            if (numChairs < 2) {
-                totalPrice = onlyPrice * numChairs;
-            } else if (numChairs > 1) {
-                totalPrice = multiPrice * numChairs;
-            }
-            console.log(totalPrice);
-            resPrice = $('<bold>', {
-                text: "Your calculated price for " + numChairs + " people is " + totalPrice + " $ .",
-                css: {
-                    fontSize: "20px",
-                    marginTop: "2rem"
-                }
-            });
-            resPrice.appendTo('.container');
-        },
-    });
-    priceBtn.appendTo(btns);
-
-    var resetBtn = $('<button>', {
-        class: "reset btn btn-default",
-        text: "Reset",
-        css: { marginLeft: "1rem" },
-        click: function(e) {
-            $('.red').removeClass('red');
-            $('bold').empty();
-        },
-    });
-    resetBtn.appendTo(btns);
-    btns.appendTo('.container');
+    gridPanel.appendTo(container);
 });
+
+function makeSeat(rowDigit, colDigit, gridPanel, seatName) {
+    var ABCArray = []
+    if (seatName === 'Aisle') {
+        return divMaker(gridPanel, '');
+    }
+    if (seatName === 'Col') {
+        return divMaker(gridPanel, seatName + ': ' + rowDigit);
+    }
+    return divMaker(gridPanel, seatName + ': ' + rowDigit + '-' + colDigit);
+}
+
+function setAlert(ColOrRow, Msg) {
+    ColOrRow.parent().parent().addClass('has-error');
+    $('<div>', {
+        text: Msg,
+    }).addClass('alert alert-danger row').insertAfter($('form')).fadeTo(3000, 500).slideUp(500, function() {
+        $(this).slideUp(500);
+        setTimeout(function() {
+            $('.alert-danger').remove();
+            ColOrRow.parent().parent().removeClass('has-error');
+        }, 3000);
+    });
+}
+
+function genCharArray(charA, charZ) {
+    var a = [],
+        i = charA.charCodeAt(0),
+        j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return JSON.stringify(a);
+}
+// console.log(genCharArray('a', 'z'));
+
+function divMaker(gridPanel, text) {
+    var widthPX = Math.ceil((container.width() * 0.975) / numColumns) - 2;
+    if (numColumns > 7) {
+        widthPX = Math.ceil((container.width() * 0.975) / numColumns) - 2;
+        // + Math.ceil((container.width() / (container.width() / 100)) * 2);
+    }
+    console.log('container: ' + container.width());
+    console.log('numColumns: ' + numColumns);
+    console.log('widthPX: ' + widthPX);
+    return $('<div>', {
+        text: text,
+        class: "seat",
+        click: function(e) {
+            e.preventDefault();
+        },
+        width: widthPX,
+    }).appendTo(gridPanel);
+}
