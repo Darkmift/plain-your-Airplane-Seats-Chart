16204753f42309571eb30067452d0ff7ccb4b5b7
Darkmift <Darkmift@users.noreply.github.com>
2018-04-14 12:18:02 +0300

    visual logic,moved prices to bottom,cols fixed some tweaks to visual

--- graveyard.js
+++ graveyard.js
@@ -1,4 +1,6 @@
 //chunk id 7
+console.log(idIndexString);
+console.log('seat: ' + seatsToBuy[index].name + 'and ' + seatsToBuy[index + 1].name + ' are next to each other');
 var idIndexString = $('#' + index).length ? '#' + index : 0;
 console.log('idindexstring' + idIndexString);
 console.log('idindexlenght: ' + $(idIndexString).length);
@@ -9,3 +11,70 @@ if ($(idIndexString).length) {
         console.log('seat: ' + seatsToBuy[index].name + 'and ' + seatsToBuy[index + 1].name + ' are next to each other');
     }
 }
+
+//chunk id 8
+if ($(this).next().is(".red")) {
+    $(this).attr("price", 200);
+    $(this).next().attr("price", 200);
+    //console.log('this: ' + $(this).attr("price"), 'next: ', $(this).next().attr("price"));
+    //console.log($(this)[0].attributes.price.value);
+}
+if ($(this).prev().is(".red")) {
+    $(this).attr("price", 200);
+    $(this).prev().attr("price", 200);
+    //console.log('this: ' + $(this).attr("price"), 'prev: ' + $(this).prev().attr("price"));
+    //console.log($(this)[0].attributes.price.value);
+}
+
+//chunk 9
+function divMaker(gridPanel, text) {
+    var widthPX = Math.ceil((container.width() * 0.985) / numColumns) - 1;
+    if (numColumns > 7) {
+        widthPX = Math.ceil((container.width() * 0.985) / numColumns) - 1;
+    }
+    console.log('widthPX: ' + widthPX);
+    var div = $('<div>', {
+        text: text,
+        class: "seat",
+        click: function(e) {
+            e.preventDefault();
+        },
+        width: widthPX,
+    });
+    if (~text.indexOf("Seat")) {
+        return $('<div>', {
+            text: text,
+            class: "seat",
+            price: 100,
+            leftOfIsle: false,
+            click: function(e) {
+                e.preventDefault();
+                console.log('clicked on price:' + $(this).attr("price"));
+                var choseChair = $(event.target);
+                choseChair.toggleClass('red');
+                //graveyard chunk id 8
+            },
+            width: widthPX,
+        }).appendTo(gridPanel);
+    }
+    if (~text.indexOf("Column")) {
+        return div.addClass('colHead').appendTo(gridPanel);
+    }
+    if (text === '') {
+        return div.addClass('aisle').appendTo(gridPanel);
+    }
+    return div.appendTo(gridPanel);
+}
+
+//graveyard chunk id 11
+console.log('indexPlusOne: ' + indexPlusOne);
+console.log('seatsToBuy[index].id: ' + seatsToBuy[index].id);
+console.log(seatsToBuy[indexPlusOne].id + '<<the +1/ and>>' + seatsToBuy[index].id);
+
+//graveyard chunk id 12
+totalDiv = $('<b>', {
+    text: totalPrice,
+}).css({
+    'background-color': 'white',
+    'border-radius': '2px'
+});
--- main.js
+++ main.js
@@ -13,6 +13,17 @@ const winVW = $('#SeatChartContainer').width();
 //init seat row
 let SeatRow;
 var widthPX;
+
+//resize fix
+$(window).resize(function() {
+    console.log("<div>Handler for .resize() called.</div>");
+    //remove previous chart if any
+    if (!$('.grid-panel').is(':empty')) {
+        $('.grid-panel').remove();
+        $('.alert').remove();
+    }
+});
+
 $("form").submit(function(e) {
     //prevent submit
     e.preventDefault();
@@ -24,6 +35,20 @@ $("form").submit(function(e) {
     numColumns = Number(Column.val());
     numRows = Number(Row.val());
 
+    //validation error alert div function
+    function setAlert(ColOrRow, Msg) {
+        ColOrRow.parent().parent().addClass('has-error');
+        $('<div>', {
+            text: Msg,
+        }).addClass('alert alert-danger row').insertAfter($('form')).fadeTo(3000, 500).slideUp(500, function() {
+            $(this).slideUp(500);
+            setTimeout(function() {
+                $('.alert-danger').remove();
+                ColOrRow.parent().parent().removeClass('has-error');
+            }, 3000);
+        });
+    }
+
     //validate Columns
     switch (true) {
         case (numColumns > 10):
@@ -32,13 +57,6 @@ $("form").submit(function(e) {
         case (numColumns < 4):
             setAlert(Column, '4 columns is the min allowed.');
             break;
-        default:
-            break;
-
-    }
-
-    //validate Rows
-    switch (true) {
         case (numRows < 10):
             setAlert(Row, '10 rows is the min allowed.');
             break;
@@ -61,17 +79,13 @@ $("form").submit(function(e) {
 
     //remove previous chart if any
     if ($('.grid-panel')) {
-        $('.grid-panel').remove();
+        $('.grid-panel').empty();
         $('.alert').remove();
     }
 
-    //make panel grid
-    let gridPanel = $('<div>').addClass("grid-panel").css({
-        // "border": "1px solid black",
-        // 'margin': '3px',
-        "clear": "both"
-    });
-
+    // //make panel grid
+    let gridPanel = $('<div>').addClass("grid-panel pre-scrollable");
+    let colhead = $('#SeatChartContainer').addClass("grid-panel");
     switch (numColumns) {
         case 4:
             SeatRow = 'SS_SS';
@@ -105,15 +119,15 @@ $("form").submit(function(e) {
     console.log(numColumns);
     var seatRowNum = 1;
     var seatNameNum = 1;
-
+    //create the plane columns
     for (let index = 0; index < SeatRow.length; index++) {
         // console.log(SeatRow.charAt(index) + index);
         switch (SeatRow.charAt(index)) {
             case 'S':
-                makeSeat(seatRowNum, seatNameNum, gridPanel, 'Column');
+                makeSeat(seatRowNum, seatNameNum, colhead, 'Column').css('top', $(this).scrollTop() + "px");
                 break;
             case '_':
-                makeSeat(seatRowNum, seatNameNum, gridPanel, 'Aisle');
+                makeSeat(seatRowNum, seatNameNum, colhead, 'Aisle').css('top', $(this).scrollTop() + "px");
                 seatNameNum--;
                 break;
         }
@@ -135,6 +149,7 @@ $("form").submit(function(e) {
             }
             seatNameNum++;
         }
+        // added hr for visual seperation
         $('<hr>').css({
             "width": "100%",
             "height": "1px",
@@ -147,16 +162,121 @@ $("form").submit(function(e) {
     console.log('container: ' + container.width());
     console.log('numColumns: ' + numColumns);
     gridPanel.appendTo(container);
+
+    ///PRICING and btns
+    ///pricing stuff
+    var btns = $('<div>', {
+        class: "input-group-btn",
+        css: {
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "1rem"
+        },
+    });
+
+    var priceBtn = $('<button>', {
+        class: "price btn btn-default",
+        text: "Price",
+        css: { marginRight: "1rem" },
+        click: function(e) {
+
+            //remove previous calc
+            $('.alert').remove();
+            chosenSeats = $('*.red');
+            var numChairs = Number(chosenSeats.length);
+            //init selected seats
+            var seatsToBuy = [];
+            var chosenSeatsId = [];
+            var chosenSeatsName = [];
+            //grab id of all selected seats and add to chosenSeatsId array as numbers
+            for (let i = 0; i < chosenSeats.length; i++) {
+                seatsToBuy[i] = {
+                    id: Number(chosenSeats[i].attributes.id.value),
+                    name: chosenSeats[i].innerText,
+                    price: 100
+                }
+                chosenSeatsId[i] = Number(chosenSeats[i].attributes.id.value);
+                chosenSeatsName[i] = chosenSeats[i].innerText;
+            }
+            console.log(seatsToBuy);
+            //set cumulative price
+            for (let index = 0; index < seatsToBuy.length; index++) {
+                //init +1counter set to 0 if num is greater than array length
+                var indexPlusOne = (index == seatsToBuy.length - 1 ? 0 : index + 1);
+                //ignore if seat is 10th in row
+                if (seatsToBuy[index].id % 10 !== 0) {
+                    //check if seats next to each other and seat(i) is not left of aisle
+                    if (index < seatsToBuy.length) {
+                        //graveyard chunk id 11
+                        if ((seatsToBuy[indexPlusOne].id - seatsToBuy[index].id) === 1) {
+                            //graveyard chunk Id 7
+                            console.log('#' + seatsToBuy[index].id);
+                            if (!$('#' + seatsToBuy[index].id).next().is(".aisle")) {
+                                //console.log('seat: ' + seatsToBuy[index].name + ' is left of isle');
+                                console.log('seat: ' + seatsToBuy[index].name + 'and ' + seatsToBuy[index + 1].name + ' are next to each other');
+                                seatsToBuy[index].price = 200;
+                                seatsToBuy[index + 1].price = 200;
+                            }
+                        }
+                    }
+                } else {
+                    //console.log('is %10: ' + seatsToBuy[index].id);
+                }
+            }
+            //calc total cost
+            var totalPrice = 0;
+            var seatTextBillDL = $('<dl>');
+            var seatTextBillDT = $('<dt>', {
+                text: 'Bill BreakDown',
+            }).appendTo(seatTextBillDL);
+
+            for (let index = 0; index < seatsToBuy.length; index++) {
+                console.log('seat: ' + seatsToBuy[index].name + ' Price of seat: ' + seatsToBuy[index].price);
+                seatTextBillDL.append($('<dd>', {
+                    text: 'seat: ' + seatsToBuy[index].name + ' Price of seat: ' + seatsToBuy[index].price,
+                }));
+                //seatTextBill = seatTextBill + '<dd>seat: ' + seatsToBuy[index].name + ' Price of seat: ' + seatsToBuy[index].price + '<dd>';
+                totalPrice = totalPrice + seatsToBuy[index].price;
+            }
+            console.log('totalPrice: ' + totalPrice);
+            //graveyard chunk id 12
+            resPrice = $('<div>', {
+                html: '<b style="background-color:white;border-radius:2px;"> Your calculated price for ' + numChairs + " seats is " + totalPrice + " $ . " + '</b>',
+                css: {
+                    fontSize: "20px",
+                    marginTop: "2rem"
+                }
+            }).addClass('alert alert-success');
+            seatTextBillDL.appendTo(resPrice);
+            resPrice.appendTo(container);
+            window.scrollTo(0, document.body.scrollHeight);
+        },
+    });
+    priceBtn.appendTo(btns);
+
+    var resetBtn = $('<button>', {
+        class: "reset btn btn-default",
+        text: "Reset",
+        css: { marginLeft: "1rem" },
+        click: function(e) {
+            $('.red').removeClass('red');
+            $('.alert').remove();
+        },
+    });
+    //make panel grid
+    resetBtn.appendTo(btns);
+    colhead.append(btns);
 });
 
 function makeSeat(rowDigit, colDigit, gridPanel, seatName) {
-    var ABCArray = []
     if (seatName === 'Aisle') {
         return divMaker(gridPanel, '').css('border', '1px solid transparent!important');
     }
     if (seatName === 'Column') {
         return divMaker(gridPanel, seatName + ': ' + colDigit);
     }
+    //if not column or aisle...
     //assign id to seats
     var SeatId = rowDigit.toString() + colDigit.toString();
     if (colDigit === 10) {
@@ -165,60 +285,24 @@ function makeSeat(rowDigit, colDigit, gridPanel, seatName) {
     return divMaker(gridPanel, seatName + ': ' + rowDigit + '-' + colDigit).attr('id', SeatId);
 }
 
-function setAlert(ColOrRow, Msg) {
-    ColOrRow.parent().parent().addClass('has-error');
-    $('<div>', {
-        text: Msg,
-    }).addClass('alert alert-danger row').insertAfter($('form')).fadeTo(3000, 500).slideUp(500, function() {
-        $(this).slideUp(500);
-        setTimeout(function() {
-            $('.alert-danger').remove();
-            ColOrRow.parent().parent().removeClass('has-error');
-        }, 3000);
-    });
-}
 
+//graveyard chunk id 9 old divmaker()
 function divMaker(gridPanel, text) {
-    var widthPX = Math.ceil((container.width() * 0.985) / numColumns);
-    if (numColumns > 7) {
-        widthPX = Math.ceil((container.width() * 0.985) / numColumns);
-    }
-    console.log('widthPX: ' + widthPX);
+    var widthPX = Math.ceil((container.width() * 0.985) / numColumns) - 2;
+    //console.log('widthPX: ' + widthPX);
     var div = $('<div>', {
         text: text,
         class: "seat",
         click: function(e) {
             e.preventDefault();
-        },
-        width: widthPX,
-    });
-    if (~text.indexOf("Seat")) {
-        return $('<div>', {
-            text: text,
-            class: "seat",
-            price: 100,
-            leftOfIsle: false,
-            click: function(e) {
-                e.preventDefault();
-                console.log('clicked on price:' + $(this).attr("price"));
+            if (~$(this).text().indexOf("Seat")) {
+                //console.log('clicked on price:' + $(this).attr("price"));
                 var choseChair = $(event.target);
                 choseChair.toggleClass('red');
-                // if ($(this).next().is(".red")) {
-                //     $(this).attr("price", 200);
-                //     $(this).next().attr("price", 200);
-                //     //console.log('this: ' + $(this).attr("price"), 'next: ', $(this).next().attr("price"));
-                //     //console.log($(this)[0].attributes.price.value);
-                // }
-                // if ($(this).prev().is(".red")) {
-                //     $(this).attr("price", 200);
-                //     $(this).prev().attr("price", 200);
-                //     //console.log('this: ' + $(this).attr("price"), 'prev: ' + $(this).prev().attr("price"));
-                //     //console.log($(this)[0].attributes.price.value);
-                // }
-            },
-            width: widthPX,
-        }).appendTo(gridPanel);
-    }
+            }
+        },
+        width: widthPX,
+    }).appendTo(gridPanel);
     if (~text.indexOf("Column")) {
         return div.addClass('colHead').appendTo(gridPanel);
     }
@@ -227,140 +311,3 @@ function divMaker(gridPanel, text) {
     }
     return div.appendTo(gridPanel);
 }
-
-
-///pricing stuff
-var btns = $('<div>', {
-    class: "input-group-btn",
-    css: {
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        marginTop: "1rem"
-    },
-});
-
-var priceBtn = $('<button>', {
-    class: "price btn btn-default",
-    text: "Price",
-    css: { marginRight: "1rem" },
-    click: function(e) {
-
-        //remove previous calc
-        $('bold').remove();
-        $('.alert').remove();
-
-        var basePrice = 100;
-        var multiPrice = 200;
-        var totalPrice = 0;
-
-        chosenSeats = $('*.red');
-        var numChairs = Number(chosenSeats.length);
-        //init selected seats
-        var seatsToBuy = [];
-        var chosenSeatsId = [];
-        var chosenSeatsName = [];
-        //grab id of all selected seats and add to chosenSeatsId array as numbers
-        for (let i = 0; i < chosenSeats.length; i++) {
-            seatsToBuy[i] = {
-                id: Number(chosenSeats[i].attributes.id.value),
-                name: chosenSeats[i].innerText,
-                price: basePrice
-            }
-            chosenSeatsId[i] = Number(chosenSeats[i].attributes.id.value);
-            chosenSeatsName[i] = chosenSeats[i].innerText;
-        }
-        console.log(seatsToBuy);
-        //set cumulative price
-        for (let index = 0; index < seatsToBuy.length; index++) {
-            //init +1counter set to 0 if num is greater than array length
-            var indexPlusOne = (index == seatsToBuy.length - 1 ? 0 : index + 1);
-            //ignore if seat is 10th in row
-            if (seatsToBuy[index].id % 10 !== 0) {
-                //check if seats next to each other and seat(i) is not left of aisle
-                if (index < seatsToBuy.length) {
-                    console.log('indexPlusOne: ' + indexPlusOne);
-                    // console.log('seatsToBuy[index].id: ' + seatsToBuy[index].id);
-                    // console.log(seatsToBuy[indexPlusOne].id + '<<the +1/ and>>' + seatsToBuy[index].id);
-                    if ((seatsToBuy[indexPlusOne].id - seatsToBuy[index].id) === 1) {
-                        //console.log(idIndexString);
-                        // console.log('seat: ' + seatsToBuy[index].name + 'and ' + seatsToBuy[index + 1].name + ' are next to each other');
-                        //graveyard chunk Id 7
-                        console.log('#' + seatsToBuy[index].id);
-                        if (!$('#' + seatsToBuy[index].id).next().is(".aisle")) {
-                            //console.log('seat: ' + seatsToBuy[index].name + ' is left of isle');
-                            console.log('seat: ' + seatsToBuy[index].name + 'and ' + seatsToBuy[index + 1].name + ' are next to each other');
-                            seatsToBuy[index].price = 200;
-                            seatsToBuy[index + 1].price = 200;
-                        }
-                    }
-                }
-            } else {
-                //console.log('is %10: ' + seatsToBuy[index].id);
-            }
-        }
-        //calc total cost
-        var totalPrice = 0;
-        var seatTextBillDL = $('<dl>');
-        var seatTextBillDT = $('<dt>', {
-            text: 'Bill BreakDown',
-        }).appendTo(seatTextBillDL);
-
-        for (let index = 0; index < seatsToBuy.length; index++) {
-            console.log('seat: ' + seatsToBuy[index].name + ' Price of seat: ' + seatsToBuy[index].price);
-            seatTextBillDL.append($('<dd>', {
-                text: 'seat: ' + seatsToBuy[index].name + ' Price of seat: ' + seatsToBuy[index].price,
-            }));
-            //seatTextBill = seatTextBill + '<dd>seat: ' + seatsToBuy[index].name + ' Price of seat: ' + seatsToBuy[index].price + '<dd>';
-            totalPrice = totalPrice + seatsToBuy[index].price;
-        }
-        console.log('totalPrice: ' + totalPrice);
-
-
-
-        //console.log(totalPrice);
-        resPrice = $('<div>', {
-            text: "Your calculated price for " + numChairs + " people is " + totalPrice + " $ .",
-            css: {
-                fontSize: "20px",
-                marginTop: "2rem"
-            }
-        }).addClass('alert alert-success');
-        seatTextBillDL.appendTo(resPrice);
-        resPrice.appendTo(container);
-        window.scrollTo(0, document.body.scrollHeight);
-    },
-});
-priceBtn.appendTo(btns);
-
-var resetBtn = $('<button>', {
-    class: "reset btn btn-default",
-    text: "Reset",
-    css: { marginLeft: "1rem" },
-    click: function(e) {
-        $('.red').removeClass('red');
-        $('bold').empty();
-    },
-});
-resetBtn.appendTo(btns);
-btns.appendTo(container);
-
-
-///test
-var contents = [
-    { name: 'a', type: 1 },
-    { name: 'b', type: 5 },
-    { name: 'c', type: 2 },
-    { name: 'd', type: 1 }
-];
-
-contents.map((content, index, array) => {
-    switch (content.type) {
-        case 1:
-            console.log("type is one and next type is: ", array[index + 1] ? array[index + 1].type : 'empty');
-            break;
-        case 2:
-            console.log("type is two")
-            break;
-    }
-});
